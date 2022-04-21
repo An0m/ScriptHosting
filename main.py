23277263ac0e003034802cf1ffda6e4deca9a946
An0m <68523000+An0m@users.noreply.github.com>
2022-04-21 21:54:41 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-import uuid
-
-for i in range(25):
-    with open(str(uuid.uuid4()) + ".txt", "w") as f:
-        f.write(''.join(secrets.choice(string.ascii_uppercase + string.digits) for i in range(200)))
